c476a4e8469d629aa375b2d285b36bc3988e0632
Lisa Pianezzi <pianelis@students.zhaw.ch>
2025-04-23 11:29:43 +0200

    Gitignore und utils zu V3 ausgetauscht

--- Start.py
+++ Start.py
@@ -7,7 +7,7 @@ st.title("Lernapp")
 
 # Streamlit über den Text unten direkt in die App - cool!
 """
-In dieser Lernapp werden 10 Single-Choice Fragen zum Thema Parasitologie aus dem Modul Medizinische Mikrobiologie 2 in Form eines Quizzes gestellt.
+In dieser Lernapp werden 10 Single-Choice Fragen zum Thema Parasitologie aus dem Modul Medizinische Mikrobiologie 2 in Form eines Quizzes gestellt!
 
 Diese App wurde von folgenden Personen entwickelt:
 - Lisa Pianezzi (pianelis@students.zhaw.ch)
