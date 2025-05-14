5ef3ddbafc6d1d86ff57d6c4b551b7ef45088262
wedermichelle <michelle.weder@bluewin.ch>
2025-05-14 11:04:08 +0200

    Gerüst Statistik und Grafik

---
+++ pages/3_Statistik.py
@@ -0,0 +1,17 @@
+from utils.login_manager import LoginManager
+LoginManager().go_to_login('Start.py') 
+
+import streamlit as st
+
+st.title('BMI Werte')
+
+data_df = st.session_state['data_df']
+if data_df.empty:
+    st.info('Keine BMI Daten vorhanden. Berechnen Sie Ihren BMI auf der Startseite.')
+    st.stop()
+
+# Sort dataframe by timestamp
+data_df = data_df.sort_values('timestamp', ascending=False)
+
+# Display table
+st.dataframe(data_df)
